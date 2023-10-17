7deff6bb613da589916a326620e03440ef1bdd91
MarqDevSilva <douglas.tecnico.info@gmail.com>
2023-10-17 15:52:45 -0300

    Criando Modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'creator', loadChildren: () => import('./creator-event/creator-event.module').then(m => m.CreatorEventModule) },
+  { path: 'attendee', loadChildren: () => import('./subscriber-event/subscriber-event.module').then(m => m.SubscriberEventModule) },
+  { path: 'event/:id', loadChildren: () => import('./page-event/page-event.module').then(m => m.PageEventModule) },
+  { path: '', redirectTo: 'creator', pathMatch: 'full' }, // Redirecione para a área do criador por padrão
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SubscriberNavbarComponent } from './shared/components/subscriber-navbar/subscriber-navbar.component';
-import { CreatorNavbarComponent } from './shared/components/creator-navbar/creator-navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubscriberNavbarComponent,
-    CreatorNavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
