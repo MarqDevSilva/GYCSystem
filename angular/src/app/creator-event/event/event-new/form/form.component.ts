fbe5f8c43e624208c96b6b234b176c62a3d8e498
MarqDevSilva <douglas.tecnico.info@gmail.com>
2023-10-25 11:47:47 -0300

    Formulário Dinamico Ok

--- angular/src/app/creator-event/event/event-new/form/form.component.ts
+++ angular/src/app/creator-event/event/event-new/form/form.component.ts
@@ -17,55 +17,20 @@ export class FormComponent {
 
   camposAdicionais: any[] = [];
 
-  //Variaveis para campo lista
-  select: string[] = [];
-  addSelect: string = '';
+  newSelect(index: number){
+    if(this.camposAdicionais[index].onAdd){
+      this.camposAdicionais[index].options.push(this.camposAdicionais[index].onAdd);
+      this.camposAdicionais[index].onAdd = '';
+    }
+    console.log(this.camposAdicionais)
+  }
 
-  //Variaveis para escolha única
-  radio: string[] = [];
-  addRadio: string = '';
-
-  //Variaveis para escolha mútipla
-  checkBox: string[] = [];
-  addCheck: string = '';
-
-  //Lógica para campo lista
-  newSelect(){
-    if(this.addSelect){
-      this.select.push(this.addSelect);
-      this.addSelect = '';
+  removeSelect(inputIndex: number, optionIndex: number) {
+    if(this.camposAdicionais[inputIndex] && this.camposAdicionais[inputIndex].options) {
+      this.camposAdicionais[inputIndex].options.splice(optionIndex, 1);
     }
   }
 
-  removeSelect(index: number) {
-    this.select.splice(index, 1);
-  }
-
-  //Lógica escolha única
-  newRadio(){
-    if(this.addRadio){
-      this.radio.push(this.addRadio);
-      this.addRadio = '';
-    }
-  }
-
-  removeRadio(index: number) {
-    this.radio.splice(index, 1);
-  }
-
-  //Lógica para checkBox
-  newCheck(){
-    if(this.addCheck){
-      this.checkBox.push(this.addCheck);
-      this.addCheck = '';
-    }
-  }
-
-  removeCheck(index: number) {
-    this.checkBox.splice(index, 1);
-  }
-
-  //Funções para adicionar os campos
   onAdd(type: string){
     if(type === "radio" || type === "check" || type === "list"){
       this.camposAdicionais.push({type: type, ask: '', required: false, options: []})
