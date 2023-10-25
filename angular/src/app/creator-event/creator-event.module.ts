831ab899c2b962f04e78599a3644a2d48274a71b
MarqDevSilva <douglas.tecnico.info@gmail.com>
2023-10-25 10:36:54 -0300

    NgFor para renderizar os campos

--- angular/src/app/creator-event/creator-event.module.ts
+++ angular/src/app/creator-event/creator-event.module.ts
@@ -1,33 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatDividerModule} from '@angular/material/divider';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
+
+
 import { CreatorEventRoutingModule } from './creator-event-routing.module';
 import { EventListComponent } from './event/event-list/event-list.component';
 import { AccommodationComponent } from './event/event-new/accommodation/accommodation.component';
 import { EventNewComponent } from './event/event-new/event-new.component';
+import { FormComponent } from './event/event-new/form/form.component';
 import { InfoBasicComponent } from './event/event-new/info-basic/info-basic.component';
 import { PayMethodComponent } from './event/event-new/pay-method/pay-method.component';
+import { SnackComponent } from './event/event-new/snack/snack.component';
 import { CreatorFooterComponent } from './shrared/components/creator-footer/creator-footer.component';
 import { CreatorNavBarComponent } from './shrared/components/creator-nav-bar/creator-nav-bar.component';
-import { SnackComponent } from './event/event-new/snack/snack.component';
-import { FormComponent } from './event/event-new/form/form.component';
 
 
 
@@ -45,6 +48,8 @@ import { FormComponent } from './event/event-new/form/form.component';
   ],
   imports: [
     CommonModule,
+    MatSidenavModule,
+    MatListModule,
     MatDividerModule,
     MatSelectModule,
     MatCheckboxModule,
--- angular/src/app/creator-event/event/event-new/form/form.component.ts
+++ angular/src/app/creator-event/event/event-new/form/form.component.ts
@@ -7,4 +7,74 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
 
+  camposPadrao: any[] = [
+    {type: 'short-text', ask: 'Nome Completo', required: true},
+    {type: 'short-text', ask: 'E-mail', required: true},
+    {type: 'short-text', ask: 'Tipo de Documento', required: true},
+    {type: 'short-text', ask: 'Nº Documento', required: true},
+    {type: 'short-text', ask: 'Celular', required: true}
+  ];
+
+  camposAdicionais: any[] = [];
+
+  //Variaveis para campo lista
+  select: string[] = [];
+  addSelect: string = '';
+
+  //Variaveis para escolha única
+  radio: string[] = [];
+  addRadio: string = '';
+
+  //Variaveis para escolha mútipla
+  checkBox: string[] = [];
+  addCheck: string = '';
+
+  //Lógica para campo lista
+  newSelect(){
+    if(this.addSelect){
+      this.select.push(this.addSelect);
+      this.addSelect = '';
+    }
+  }
+
+  removeSelect(index: number) {
+    this.select.splice(index, 1);
+  }
+
+  //Lógica escolha única
+  newRadio(){
+    if(this.addRadio){
+      this.radio.push(this.addRadio);
+      this.addRadio = '';
+    }
+  }
+
+  removeRadio(index: number) {
+    this.radio.splice(index, 1);
+  }
+
+  //Lógica para checkBox
+  newCheck(){
+    if(this.addCheck){
+      this.checkBox.push(this.addCheck);
+      this.addCheck = '';
+    }
+  }
+
+  removeCheck(index: number) {
+    this.checkBox.splice(index, 1);
+  }
+
+  //Funções para adicionar os campos
+  onAdd(type: string){
+    if(type === "radio" || type === "check" || type === "list"){
+      this.camposAdicionais.push({type: type, ask: '', required: false, options: []})
+    }else{
+      this.camposAdicionais.push({type: type, ask: '', required: false})
+    }
+  }
+
+  onDelete(index: number){
+    this.camposAdicionais.splice(index, 1);
+  }
 }
