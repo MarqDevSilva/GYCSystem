62ae271d5a12cdfdfb89231596ab328a34ddc2a6
MarqDevSilva <douglas.tecnico.info@gmail.com>
2023-11-27 10:46:19 -0300

    New feature

--- angular/src/app/creator-event/event/event-new/page/page.component.ts
+++ angular/src/app/creator-event/event/event-new/page/page.component.ts
@@ -1,16 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
-import { SobreComponent } from './sobre/sobre.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { Component, Output, ViewChild } from '@angular/core';
 import { CapaComponent } from './capa/capa.component';
-import { ProgramacaoComponent } from './programacao/programacao.component';
 import { LocalComponent } from './local/local.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { ProgramacaoComponent } from './programacao/programacao.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent {
+export class PageComponent{
+
+  @Output() capa = this.formBuilder.group({});
+
+  constructor(private formBuilder: FormBuilder){}
+
 
   @ViewChild(CapaComponent) capaComponent?: CapaComponent;
   @ViewChild(SobreComponent) sobreComponent?: SobreComponent;
